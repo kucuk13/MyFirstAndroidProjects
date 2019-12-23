3b83b9a25a2e98e0e696b52e21aaf05173d7fb86
kucuk13 <52529079+kucuk13@users.noreply.github.com>
2019-12-23 23:12:43 +0300

    Adding two numbers.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.WindowManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +18,19 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        final EditText x = findViewById(R.id.a);
+        final EditText y = findViewById(R.id.b);
+        final TextView z = findViewById(R.id.c);
+        Button add = findViewById(R.id.bt);
+        add.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(x.getText().toString());
+                int b = Integer.parseInt(y.getText().toString());
+                z.setText(String.valueOf(a + b));
+            }
+        });
+        
     }
 }
